793e0c7211df59d049674eea37bece7a226b7e96
Gonzalez Salvador <salvadorsopas@gmail.com>
2023-09-05 02:56:19 -0600

    login con axios & token implementado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,47 @@
 import logo from '@/assets/react.svg'
 import '@/styles/form.css' // importamos el estilo para este formulario el cual se encuentra en la carpeta styles y el archivo se llama form.css
+import { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+  const [email, setEmail] = useState('') // hook de useState
+  const [password, setPassword] = useState('')
+
+  const sendData = async (data) => {
+    const config = {
+      method: 'post',// metodo con el cual podemos mandar nuestros datos para el inicio
+      maxBodyLength: Infinity,
+      url: 'https://reactavanzadom6.onrender.com/login',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data : JSON.stringify(data)
+    }
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault() // evita que se vuelva a cargar la pag
+    sendData({ email, password })
+  }
   return (
     <main className='form-signin w-100 m-auto'>
-      <form>
+      <form onSubmit={handleSubmit}> {// accion que queremos que haga del formulario  
+}
         <img className='mb-4' src={logo} alt='React' width='72' height='57' />
         <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
 
         <div className='form-floating'>
-          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' />
+          <input type='email' className='form-control' id='floatingInput' placeholder='name@example.com' onChange={(event) => setEmail(event.target.value)} />
           <label htmlFor='floatingInput'>Email address</label>
         </div>
         <div className='form-floating'>
-          <input type='password' className='form-control' id='floatingPassword' placeholder='Password' />
+          <input type='password' className='form-control' id='floatingPassword' placeholder='Password' onChange={(event) => setPassword(event.target.value)} />
           <label htmlFor='floatingPassword'>Password</label>
         </div>
 
